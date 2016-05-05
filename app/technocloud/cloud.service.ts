f4f2cc785fe59de2e4ccb65b3ef160bd38d56356
Vidyakina ES <VidyakinaES@quarta.su>
2016-05-05 14:36:03 +0300

    Revert "Revert "first not working""
    This reverts commit 0458661715ab3a851c1cf6a2a99b18dfe4a7d5df.

---
+++ app/technocloud/cloud.service.ts
@@ -0,0 +1,15 @@
+import { Injectable } from '@angular/core';
+
+import { SkillTypes } from './mock-skillTypes'
+import { Skills } from './mock-skills'
+
+@Injectable()
+export class CloudService {
+    getSkillTypes() {
+        return Promise.resolve(SkillTypes);
+    }
+    
+    getSkills(skillTypeId: number){
+        return Promise.resolve(Skills);
+    }
+}
---
+++ app/technocloud/mock-skills.ts
@@ -0,0 +1,7 @@
+import { Skill } from '../model/Skill';
+
+export var Skills: Skill[] = [
+    {"id": 1, "name": "C#"},
+    {"id": 2, "name": "Java"},
+    {"id": 3, "name": "Python"}
+]
